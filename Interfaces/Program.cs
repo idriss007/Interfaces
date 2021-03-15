60edf6c131a2861613dd342140d35aeb4636fc89
Idris Uslu <idrisuslu.007@hotmail.com>
2021-03-15 15:10:34 +0300

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+
+            //Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal[]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal()
+            };
+
+            foreach (var customeDal in customerDals)
+            {
+                customeDal.Add();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Update(new OracleCustomerDal());
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager personManager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "İdris",
+                LastName = "Uslu",
+                Adress = "İstanbul"
+            };
+
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Halime",
+                LastName = "Uslu",
+                Departmant = "Ev Hanımı"
+            };
+
+            personManager.Add(customer);
+        }
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+    }
+
+    class Student:IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
